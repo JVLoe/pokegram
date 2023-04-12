735aaf0abf129697da670b7a2abfde6301afe688
Johanna Loe <108595341+JVLoe@users.noreply.github.com>
2023-04-12 11:24:36 +0100

    PokeSplash's button now routes to PokeDirection page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import PokeSplash from "./Components/PokeSplash/PokeSplash";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import PokeDirection from "./Components/PokeDirection/PokeDirection";
 
 const App = () => {
   return (
     <div className="App">
-      <PokeSplash />
+        <BrowserRouter>
+            <Routes>
+                <Route index="/" element={<PokeSplash />} />
+                <Route path="direction" element={<PokeDirection />} />
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
--- src/Components/PokeSplash/PokeSplash.js
+++ src/Components/PokeSplash/PokeSplash.js
@@ -1,8 +1,12 @@
 import './PokeSplash.css'
 import Navbar from "../Narbar/Navbar";
+import {useNavigate} from "react-router-dom";
 
 
 const PokeSplash = () => {
+    const navigate = useNavigate();
+    const handleClick = () => navigate('/direction');
+
     return (
         <div className="pokeSplash">
             <Navbar />
@@ -11,9 +15,9 @@ const PokeSplash = () => {
                 </div>
                 <div className="pokeSplashTextAndButton">
                     <p className="pokeSplashText">Pokégram is a safe place for <span className="textEmphasis">all</span> Pokémon. <span className="textEmphasis">No trainers allowed.</span></p>
-                    <button className="pokeSplashButton">
-                        Enter
-                    </button>
+                        <button type="button"  onClick={handleClick} className="pokeSplashButton">
+                            Enter
+                        </button>
                 </div>
             </div>
         </div>
