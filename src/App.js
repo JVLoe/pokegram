1b64ae7aa3e97367ec67d7375d788b68458ff27d
Johanna Loe <108595341+JVLoe@users.noreply.github.com>
2023-04-12 13:00:50 +0100

    working routing. Will attempt authentication next

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import PokeSplash from "./Components/PokeSplash/PokeSplash";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import PokeDirection from "./Components/PokeDirection/PokeDirection";
 
-PokeSplash.handleClick = undefined;
 const App = () => {
   return (
     <div className="App">
         <BrowserRouter>
             <Routes>
                 <Route index="/" element={<PokeSplash />} />
-                <Route path="direction" element={<PokeDirection />}></Route>
+                <Route path="/direction" element={<PokeDirection />} />
             </Routes>
         </BrowserRouter>
     </div>
