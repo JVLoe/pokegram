b9a3e40a1f8fb15b2e3edde4aa6758f7b44fbce1
Johanna Loe <108595341+JVLoe@users.noreply.github.com>
2023-04-12 13:25:51 +0100

    authentication set up for directions page and prevents users from accessing it without clicking the button on PokeSplash. NEXT: the 404

--- src/Components/PokeSplash/PokeSplash.js
+++ src/Components/PokeSplash/PokeSplash.js
@@ -2,10 +2,12 @@ import './PokeSplash.css'
 import Navbar from "../Narbar/Navbar";
 import {useNavigate} from "react-router-dom";
 
-
-const PokeSplash = () => {
+const PokeSplash = ({ handleAuthentication }) => {
     const navigate = useNavigate();
-    const handleClick = () => navigate('/direction');
+    const handleClick = () => {
+        handleAuthentication();
+        navigate('/direction');
+    };
 
     return (
         <div className="pokeSplash">
@@ -15,13 +17,13 @@ const PokeSplash = () => {
                 </div>
                 <div className="pokeSplashTextAndButton">
                     <p className="pokeSplashText">Pokégram is a safe place for <span className="textEmphasis">all</span> Pokémon. <span className="textEmphasis">No trainers allowed.</span></p>
-                        <button type="button"  onClick={handleClick} className="pokeSplashButton">
-                            Enter
-                        </button>
+                    <button type="button"  onClick={handleClick} className="pokeSplashButton">
+                        Enter
+                    </button>
                 </div>
             </div>
         </div>
     )
 }
 
-export default PokeSplash
+export default PokeSplash;
