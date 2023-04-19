783e889e3137ea4f59ed00ada848b5eb89dfec2b
Johanna Loe <108595341+JVLoe@users.noreply.github.com>
2023-04-19 21:14:50 +0100

    saving progress before big change

--- src/Components/PokeProfileCollection/PokeProfileCollection.js
+++ src/Components/PokeProfileCollection/PokeProfileCollection.js
@@ -1,8 +1,8 @@
 import './PokeProfileCollection.scss';
 import Navbar from "../Narbar/Navbar";
-import {useEffect, useState} from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faChevronLeft, faChevronRight, faMagnifyingGlass, faRotateLeft} from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight, faMagnifyingGlass, faRotateLeft } from "@fortawesome/free-solid-svg-icons";
 
 const PokeProfileCollection = () => {
     const [pokemons, setPokemons] = useState([]);
@@ -12,10 +12,21 @@ const PokeProfileCollection = () => {
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch(
-                `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=${offset}`
+                `https://pokeapi.co/api/v2/pokemon?limit=151&offset=${offset}`
             );
             const data = await response.json();
-            setPokemons(data.results);
+            const pokemonData = await Promise.all(
+                data.results.map(async (pokemon) => {
+                    const response = await fetch(pokemon.url);
+                    const data = await response.json();
+                    return {
+                        id: data.id,
+                        name: data.name,
+                        imageUrl: data.sprites.front_default,
+                    };
+                })
+            );
+            setPokemons(pokemonData);
         };
         fetchData();
     }, [offset]);
@@ -31,7 +42,7 @@ const PokeProfileCollection = () => {
     const handleReset = () => {
         setOffset(0);
         setSearchPokemon("");
-    }
+    };
 
     const handleSearch = (event) => {
         setSearchPokemon(event.target.value);
@@ -43,12 +54,15 @@ const PokeProfileCollection = () => {
 
     return (
         <div>
-            <Navbar/>
+            <Navbar />
             <div className="pokeProfileCollectionContainer">
-
                 <div className="searchContainer">
                     <button className="btn-search">
-                        <FontAwesomeIcon icon={faMagnifyingGlass} size="lg" style={{color: "#0b26b9",}} />
+                        <FontAwesomeIcon
+                            icon={faMagnifyingGlass}
+                            size="lg"
+                            style={{ color: "#0b26b9" }}
+                        />
                     </button>
                     <input
                         className="searchInput"
@@ -61,7 +75,10 @@ const PokeProfileCollection = () => {
 
                 <div className="resetPokemonButton">
                     <button onClick={handleReset}>
-                        <FontAwesomeIcon icon={faRotateLeft} style={{color: "#0b26b9",}} />
+                        <FontAwesomeIcon
+                            icon={faRotateLeft}
+                            style={{ color: "#0b26b9" }}
+                        />
                     </button>
                 </div>
 
@@ -69,21 +86,28 @@ const PokeProfileCollection = () => {
                     {filteredPokemons.slice(0, 6).map((pokemon) => (
                         <div key={pokemon.name} className="pokeCard">
                             <img
-                                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url
-                                    .split("/")
-                                    .slice(-2, -1)}.png`}
+                                src={pokemon.imageUrl}
                                 alt={pokemon.name}
                             />
-                            <p className="avatarName">{pokemon.name}</p>
+                            <div className="pokeInfoContainer">
+                                <p className="avatarName">{pokemon.name}</p>
+                                <p className="avatarId">id: {pokemon.id}</p>
+                            </div>
                         </div>
                     ))}
                 </div>
                 <div className="navigationButtons">
                     <button onClick={handleBack} disabled={offset === 0}>
-                        <FontAwesomeIcon icon={faChevronLeft} style={{color: "#0b26b9",}} />
+                        <FontAwesomeIcon
+                            icon={faChevronLeft}
+                            style={{ color: "#0b26b9" }}
+                        />
                     </button>
                     <button onClick={handleNext} disabled={offset >= 99994}>
-                        <FontAwesomeIcon icon={faChevronRight} style={{color: "#0b26b9",}} />
+                        <FontAwesomeIcon
+                            icon={faChevronRight}
+                            style={{ color: "#0b26b9" }}
+                        />
                     </button>
                 </div>
             </div>
