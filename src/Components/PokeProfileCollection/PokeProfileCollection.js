4bfe8b49a53ebfbfaed32c78c80c111586714d47
Johanna Loe <108595341+JVLoe@users.noreply.github.com>
2023-04-24 20:37:38 +0100

    changes: each time the pragination changes, a new fetch request is made. Although this is not practical for larger projects, it works well for my small project as I will later use the id's of each pokemon to fill fetch other data based on which id is selected (longevity = thumbs up). I've put in try, catch, and finally for error handling. Added a loading message if ever it gets stuck loading. Through constant fiddle, i found the next button continues to next after reaching the final page, so added a new states and conditions to track if im on the first or last page and disable the buttons if so.

--- src/Components/PokeProfileCollection/PokeProfileCollection.js
+++ src/Components/PokeProfileCollection/PokeProfileCollection.js
@@ -1,67 +1,127 @@
-import './PokeProfileCollection.scss';
+import React, {useState, useEffect} from "react";
+
+import "./PokeProfileCollection.scss";
 import Navbar from "../Narbar/Navbar";
-import { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronLeft, faChevronRight, faMagnifyingGlass, faRotateLeft } from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {
+    faChevronLeft,
+    faChevronRight,
+    faMagnifyingGlass,
+    faRotateLeft,
+} from "@fortawesome/free-solid-svg-icons";
 
 const PokeProfileCollection = () => {
-    const [pokemons, setPokemons] = useState([]);
-    const [offset, setOffset] = useState(0);
+    const [allPokemon, setAllPokemon] = useState([]);
+    const [displayedPokemon, setDisplayedPokemon] = useState([]);
     const [searchPokemon, setSearchPokemon] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [startPage, setStartPage] = useState(true);
+    const [finalPage, setFinalPage] = useState(false);
+
+    const errorMessage = "Something went wrong :(";
 
     useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch(
-                `https://pokeapi.co/api/v2/pokemon?limit=151&offset=${offset}`
-            );
-            const data = await response.json();
-            const pokemonData = await Promise.all(
-                data.results.map(async (pokemon) => {
-                    const response = await fetch(pokemon.url);
-                    const data = await response.json();
-                    return {
-                        id: data.id,
-                        name: data.name,
-                        imageUrl: data.sprites.front_default,
-                    };
-                })
-            );
-            setPokemons(pokemonData);
-        };
-        fetchData();
-    }, [offset]);
+        async function fetchPokemon() {
+            setIsLoading(true);
+            setError(null);
+            try {
+                const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+                const data = await res.json();
+                setAllPokemon(data.results);
+                setDisplayedPokemon(data.results.slice(0, 6));
+            } catch (err) {
+                setError(err);
+                console.log(error);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        fetchPokemon();
+    }, [error]);
 
-    const handleNext = () => {
-        setOffset(offset + 6);
-    };
-
-    const handleBack = () => {
-        setOffset(Math.max(0, offset - 6));
-    };
-
-    const handleReset = () => {
-        setOffset(0);
-        setSearchPokemon("");
-    };
+    useEffect(() => {
+        const currentStartIndex = allPokemon.indexOf(displayedPokemon[0]);
+        if (currentStartIndex === 0) {
+            setStartPage(true);
+        } else {
+            setStartPage(false);
+        }
+        if (currentStartIndex + 6 >= allPokemon.length) {
+            setFinalPage(true);
+        } else {
+            setFinalPage(false);
+        }
+    }, [allPokemon, displayedPokemon]);
 
     const handleSearch = (event) => {
         setSearchPokemon(event.target.value);
+        const filteredPokemon = allPokemon.filter((pokemon) =>
+            pokemon.name.toLowerCase().includes(event.target.value.toLowerCase())
+        );
+        if (error) {
+            return <p>{errorMessage}</p>
+        } else {
+            setDisplayedPokemon(filteredPokemon.slice(0, 6));
+        }
     };
 
-    const filteredPokemons = pokemons.filter((pokemon) =>
-        pokemon.name.toLowerCase().includes(searchPokemon.toLowerCase())
-    );
+    const handleNext = () => {
+        const currentStartIndex = allPokemon.indexOf(displayedPokemon[0]);
+        const nextPokemon = allPokemon.slice(
+            currentStartIndex + 6,
+            currentStartIndex + 12
+        );
+        if (error) {
+            return <p>{errorMessage}</p>
+        } else {
+            setDisplayedPokemon(nextPokemon);
+        }
+    };
+
+    const handleBack = () => {
+        const currentStartIndex = allPokemon.indexOf(displayedPokemon[0]);
+        const prevPokemon = allPokemon.slice(
+            currentStartIndex - 6,
+            currentStartIndex
+        );
+        if (error) {
+            return <p>{errorMessage}</p>
+        } else {
+            setDisplayedPokemon(prevPokemon);
+        }
+    };
+
+    const handleReset = () => {
+        setSearchPokemon("");
+        setDisplayedPokemon(allPokemon.slice(0, 6));
+    };
+
+
+    const displayedPokemonDetails = displayedPokemon.map((pokemon) => {
+        const id = pokemon.url.split("/")[6];
+        const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+
+        return (
+            <div key={id} className="pokeCard">
+                <img src={imageUrl} alt={pokemon.name}/>
+                <p className="avatarId">#{id}</p>
+                <p className="avatarName">{pokemon.name}</p>
+            </div>
+        );
+    });
+
 
     return (
         <div>
-            <Navbar />
+            <Navbar/>
             <div className="pokeProfileCollectionContainer">
                 <div className="searchContainer">
                     <button className="btn-search">
                         <FontAwesomeIcon
                             icon={faMagnifyingGlass}
                             size="lg"
-                            style={{ color: "#0b26b9" }}
+                            style={{color: "#0b26b9"}}
                         />
                     </button>
                     <input
@@ -77,44 +137,37 @@ const PokeProfileCollection = () => {
                     <button onClick={handleReset}>
                         <FontAwesomeIcon
                             icon={faRotateLeft}
-                            style={{ color: "#0b26b9" }}
+                            style={{color: "#0b26b9"}}
                         />
                     </button>
                 </div>
-
-                <div className="pokeCardContainer">
-                    {filteredPokemons.slice(0, 6).map((pokemon) => (
-                        <div key={pokemon.name} className="pokeCard">
-                            <img
-                                src={pokemon.imageUrl}
-                                alt={pokemon.name}
-                            />
-                            <div className="pokeInfoContainer">
-                                <p className="avatarName">{pokemon.name}</p>
-                                <p className="avatarId">id: {pokemon.id}</p>
-                            </div>
+                {isLoading ? (
+                    <p>Loading...</p>
+                ) : (
+                    <div>
+                        <div className="pokeCardContainer">
+                            {displayedPokemonDetails}
                         </div>
-                    ))}
-                </div>
-                <div className="navigationButtons">
-                    <button onClick={handleBack} disabled={offset === 0}>
-                        <FontAwesomeIcon
-                            icon={faChevronLeft}
-                            style={{ color: "#0b26b9" }}
-                        />
-                    </button>
-                    <button onClick={handleNext} disabled={offset >= 99994}>
-                        <FontAwesomeIcon
-                            icon={faChevronRight}
-                            style={{ color: "#0b26b9" }}
-                        />
-                    </button>
-                </div>
+                        <div className="navigationButtons">
+                            <button onClick={handleBack} disabled={startPage}>
+                                <FontAwesomeIcon
+                                    icon={faChevronLeft}
+                                    style={{color: "#0b26b9"}}
+                                />
+                            </button>
+                            <button onClick={handleNext} disabled={finalPage}>
+                                <FontAwesomeIcon
+                                    icon={faChevronRight}
+                                    style={{color: "#0b26b9"}}
+                                />
+                            </button>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
 };
-
 export default PokeProfileCollection;
 
 /**TODO:
@@ -123,4 +176,12 @@ export default PokeProfileCollection;
  * - create PokeProfilePage && Link each card with data to that page
  * - the PokeProfilePage needs a back button to return to the PokeProfileCollection page
  *
+ *
+ *
+ *
+ * -fetch api of 151 pokemon and store its state (pokemons) - pokemons isnt interfered with
+ * - another state to set the displayed pokemon to 6 (the next buttons will basically sift through this) - maybe "dispayedPokemons"
+ * - the search will have to use some sort of spread syntax to look through all pokemon
+ *
+ * - these need to be kept seperate to a degree - using offset with pokemons essentally over-rides the limit i've manually put in the fetch url request
  * */
