5b990f78eaebb22537452ecac029584640ce900d
Johanna Loe <108595341+JVLoe@users.noreply.github.com>
2023-04-27 16:50:17 +0100

    added PokeProfile page

---
+++ src/Components/PokeProfile/PokeProfile.js
@@ -0,0 +1,13 @@
+import Navbar from "../Narbar/Navbar";
+import './PokeProfile.scss'
+
+const PokeProfile = () => {
+    return (
+        <div>
+            <Navbar showBackButton={true} />
+
+        </div>
+    )
+}
+
+export default PokeProfile
